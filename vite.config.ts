36717071b8e73101f6d34fe33333104386f3124b
uzangroove <uzan2bay@gmail.com>
2026-04-24 07:54:30 +0300

    fix: split phaser chunk for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: './',
   server: { port: 3000 },
-  build: { target: 'es2020' }
+  build: {
+    target: 'es2020',
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ['phaser'],
+        },
+      },
+    },
+  },
 });
